d4f5274c0febe84f9104f718b3cb37059335ae6c
Nikola Kiselev <ic.nikolai.kiselev@gmail.com>
2023-10-11 11:00:00 +0200

    docs: add docstrings

--- wait.go
+++ wait.go
@@ -1,3 +1,6 @@
+// Package wait allows you to work with goroutines that may return values or
+// errors. This library simplifies error handling and provides a convenient
+// interface for gathering results from concurrent tasks.
 package wait
 
 import (
@@ -5,6 +8,7 @@ import (
 	"sync"
 )
 
+// Group wait group to process gorutines that return value and error.
 type Group[T any] struct {
 	ctx     context.Context
 	cancel  context.CancelCauseFunc
@@ -13,10 +17,12 @@ type Group[T any] struct {
 	results chan T
 }
 
+// New creates a new wait group with a background context.
 func New[T any]() *Group[T] {
 	return WithContext[T](context.Background())
 }
 
+// WithContext creates a new wait group with a parent context to control lifecycle.
 func WithContext[T any](ctx context.Context) *Group[T] {
 	gctx, cancel := context.WithCancelCause(ctx)
 
@@ -29,6 +35,7 @@ func WithContext[T any](ctx context.Context) *Group[T] {
 	}
 }
 
+// Go starts a new goroutine that executes the specified function.
 func (g *Group[T]) Go(fn func(ctx context.Context) (T, error)) {
 	g.wg.Add(1)
 	go func() {
@@ -46,6 +53,9 @@ func (g *Group[T]) Go(fn func(ctx context.Context) (T, error)) {
 	}()
 }
 
+// Gather gathers results from the executed goroutines. The function is
+// called for each result, and any errors are collected. This function blocks
+// until all goroutines have completed.
 func (g *Group[T]) Gather(gatherer func(T)) error {
 	defer g.errOnce.Do(func() {
 		g.cancel(nil)
@@ -83,6 +93,8 @@ func (g *Group[T]) Gather(gatherer func(T)) error {
 	return nil
 }
 
+// Wait return results from the executed goroutines as slice and any errors
+// encountered. This function blocks until all goroutines have completed.
 func (g *Group[T]) Wait() ([]T, error) {
 	defer g.errOnce.Do(func() {
 		g.cancel(nil)
